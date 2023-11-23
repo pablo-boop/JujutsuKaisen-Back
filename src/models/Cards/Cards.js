7dfe65f87e237fb4a7b5f1b3f298a31bfc2103ee
Pablo Couto <pablocouto@senai.aluno.br>
2023-11-22 23:08:20 -0300

    feat: made cardlist crud and validations

--- src/models/Cards/Cards.js
+++ src/models/Cards/Cards.js
@@ -15,3 +15,71 @@ export class Card {
         return uuidv4();
     }
 }
+
+export class CardList {
+    constructor() {
+        this.cards = [];
+        this.deckOne = [];
+        this.deckTwo = [];
+    }
+
+    //CRUD
+    getCards() {
+        return this.cards;
+    }
+
+    getCardById(id) {
+        const card = this.cards.find((card) => card.id == id);
+        return card;
+    }
+
+    addCard(param) {
+        this.cards.push(param);
+        return param;
+    }
+
+    updateCard(id, name, img, description, atk, def, level) {
+        const card = this.getCardById(id)
+
+        if (!card) {
+            return null;
+        }
+
+        card.name = name;
+        card.img = img;
+        card.description = description;
+        card.atk = atk;
+        card.def = def;
+        card.level = level;
+
+        return card;
+    }
+
+    deleteCard(id) {
+        this.cards = this.cards.filter((card) => card.id !== id)
+    }
+
+    //CRUD Validatons
+    isURLValida(url) {
+        if(url.match(/\.(png)$/) != null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    emptyInputs(name, img, description, atk, def, level) {
+        const name = name;
+        const img = img;
+        const description = description;
+        const atk = atk;
+        const def = def;
+        const level = level;
+
+        if(name == "" || img == "" || description == "" || atk == "" || def == "" || level == "") {
+            return true
+        } else {
+            return false
+        }
+    }
+}
