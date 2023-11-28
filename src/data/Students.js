6856e6daf805790e05d34afe5e7ef6ce50a51866
Caio Ciccarelli <caio.ciccarelli@aluno.senai.br>
2023-11-28 13:45:14 -0300

    feat: data students

---
+++ src/data/Students.js
@@ -0,0 +1,38 @@
+const students = [
+    {
+        name: "Caio Ciccarelli",
+        age: 16,
+        email: "ciccarelli.ca@gmail.com",
+        description: "Um otário aí com TDAH"
+    },
+    {
+        name: "Lucas Rios",
+        age: 17,
+        email: "lucasrios@aluno.senai.br",
+        description: ""
+    },
+    {
+        name: "Nathan Oliveira",
+        age: 17,
+        email: "nathan.henrique@aluno.senai.br",
+        description: "Sou uma pessoa introvertida mas quando me acostumo com o local e as pessoas, eu fico mais solto."
+    },
+    {
+        name: "Noah Zavickis",
+        age: 16,
+        email: "noahzavickis@aluno.senai",
+        description: "Um entusiasta de gatos e faz cosplay de lagartixa"
+    },
+    {
+        name: "Pablo Couto",
+        age: 16,
+        email: "pablocouto@aluno.senai.br",
+        description: ""
+    },
+    {
+        name: "Rafael Leal",
+        age: 16,
+        email: "rafael.leal@aluno.senai.br",
+        description: "Estudante do senai"
+    }
+]
