da6f1fc4fd8a21be937c861f2598b4388ac7d8f1
Pablo Couto <pablocouto@aluno.senai.br>
2023-12-05 14:37:45 -0300

    feat: alterações de import em routes cards

--- src/routes/cards.routes.js
+++ src/routes/cards.routes.js
@@ -8,8 +8,6 @@ import {
     createCard,
     updateCards,
     deleteCard,
-    filterCardsByAtk,
-    filterCardsByDef
 } from '../controllers/cards.controllers.js';
 
 // Criando um novo roteador
